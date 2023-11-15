d5baaa41d0cc76f8ca60e942d3cb96d272796dce
Nurs <nurrs.serkul@gmail.com>
2023-11-15 23:19:35 +0600

    added ejs

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const studentsRoute = require("./routes/students");
 const subjectsRoute = require("./routes/subjects");
 const operationsRoute = require("./routes/operations");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(morgan("common"));
 app.use(express.json());
 app.use("/students", studentsRoute);
 app.use("/operations", operationsRoute);
 app.use("/subjects", subjectsRoute);
+app.use(methodOverride("_method"));
+app.use(express.static("views"));
 
 mongoose
   .connect(
@@ -25,6 +33,9 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
+
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+  res.render(createPath("login"));
 });
